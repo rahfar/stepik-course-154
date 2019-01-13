dca310741c43c8579520e1078cb4459a1b29bed5
Rakhmatullin Farid <rahfar777@gmail.com>
2019-01-13 20:35:40 +0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   init.sh
    	new file:   web/etc/hello.py
    	modified:   web/etc/nginx.conf
    	new file:   web/hello.py

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+
