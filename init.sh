5ccf117b0ba76620098a7a8294263b6bf3388eea
Rakhmatullin Farid <rahfar777@gmail.com>
2019-01-19 03:12:01 +0300

    Task 1.9  Changes to be committed: 	modified:   init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
+sudo nginx
 
