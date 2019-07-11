8903deb4574c40bcdb5ef5600c339ae7fa4abd3a
Farid <farid.rakhmatullin@glowbyteconsulting.com>
2019-07-11 12:11:16 +0300

    add django project

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 def app(env, start_response):
     body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
-    start_response('200 OK', [('Content-Type','text/plain')])
+    start_response('200 OK', [('Content-Type', 'text/plain')])
     return body
