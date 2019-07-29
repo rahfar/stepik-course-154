39fc88a2861e6698bd0dea782decc3088c14b358
Farid <farid.rakhmatullin@glowbyteconsulting.com>
2019-07-30 00:49:33 +0300

    Changes to be committed: 	modified:   .gitignore 	modified:   etc/nginx.conf 	deleted:    etc/nginx.conf.orig 	renamed:    nginx.conf -> etc/nginx.conf.orig.stepik 	modified:   hello.py 	new file:   init.sh 	modified:   servers.sh

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+# simple wsgi example
 def app(env, start_response):
     body = [bytes(i + '\n', 'ascii') for i in env['QUERY_STRING'].split('&')]
     start_response('200 OK', [('Content-Type', 'text/plain')])
--- servers.sh
+++ servers.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-user=farid
 
-if [[ $1 == "start" ]]
-then 
-    gunicorn \
-        --config /home/${user}/web/etc/hello.py \
-        --daemon \
-        --pid /home/${user}/web/gunicorn.pid \
-        hello:app
-    echo "res $?"
-    sudo nginx -c /home/${user}/web/etc/nginx.conf
-    echo "servers started"
-    exit
-elif [[ $1 == "stop" ]]
-then
-    sudo nginx -s stop
-    kill `cat /home/${user}/web/gunicorn.pid`
-    echo "servers stoped"
-    exit
-else 
-    echo "use $0 start | stop"
-    exit
-fi
+# if [[ $1 == "start" ]]
+# then 
+#     gunicorn \
+#         --bind="0.0.0.0:8000" \
+#         --daemon \
+#         --pid /home/farid/web/gunicorn.pid \
+#         hello:app
+#     echo "res $?"
+#     sudo nginx -c /home/farid/web/etc/nginx.conf
+#     echo "servers started"
+#     exit
+# elif [[ $1 == "stop" ]]
+# then
+#     sudo nginx -s stop
+#     kill `cat /home/farid/web/gunicorn.pid`
+#     echo "servers stoped"
+#     exit
+# else 
+#     echo "use $0 start | stop"
+#     exit
+# fi
