3ba252c21bee882fd6bc79cf1f2e50de5876d945
rahfar <rahfar777@gmail.com>
2019-08-01 00:13:00 +0300

    fix gitignore

--- servers.sh
+++ servers.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-# if [[ $1 == "start" ]]
-# then 
-#     gunicorn \
-#         --bind="0.0.0.0:8000" \
-#         --daemon \
-#         --pid /home/farid/web/gunicorn.pid \
-#         hello:app
-#     echo "res $?"
-#     sudo nginx -c /home/farid/web/etc/nginx.conf
-#     echo "servers started"
-#     exit
-# elif [[ $1 == "stop" ]]
-# then
-#     sudo nginx -s stop
-#     kill `cat /home/farid/web/gunicorn.pid`
-#     echo "servers stoped"
-#     exit
-# else 
-#     echo "use $0 start | stop"
-#     exit
-# fi
+if [[ $1 == "start" ]]
+then 
+    gunicorn \
+        --bind="0.0.0.0:8000" \
+        --daemon \
+        --pid /home/farid/web/gunicorn.pid \
+        hello:app 
+    gunicorn \
+        --bind="0.0.0.0:8000" \
+        --daemon \
+        --pid /home/farid/web/gunicorn.pid \
+        hello:app 
+    echo "res $?"
+    sudo nginx
+    echo "servers started"
+    exit
+elif [[ $1 == "stop" ]]
+then
+    sudo nginx -s stop
+    kill `cat /home/farid/web/gunicorn.pid`
+    echo "servers stoped"
+    exit
+else 
+    echo "use $0 start | stop"
+    exit
+fi
