e1a0eb735121d4940d40258def3d4e80036b8e67
rahfar <rahfar777@gmail.com>
2019-08-11 03:20:22 +0300

    Changes to be committed: 	modified:   init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 user=farid
-virtualenv -p python3 myvenv
-source myvenv/bin/activate
-pip install --upgrade pip
-pip install django==2.0.0
-pip install gunicorn==19.6.0
+# virtualenv -p python3 myvenv
+# source myvenv/bin/activate
+# pip install --upgrade pip
+# pip install django==2.0.0
+# pip install gunicorn==19.6.0
 #----------------------------------------------------
 sudo nginx -c /home/${user}/web/config/nginx.conf
 gunicorn \
     --bind="0.0.0.0:8000" \
     --daemon \
-    --pid /home/farid/web/gunicorn.pid \
+    --pid /home/${user}/web/gunicorn.pid \
     hello:app
 
