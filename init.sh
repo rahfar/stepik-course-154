4b86be785b7a2e214b7ef07e2109d5ce7bdb76ab
rahfar <rahfar777@gmail.com>
2019-11-04 21:28:34 +0300

    new file:   .gitignore 	modified:   init.sh 	new file:   requirements.txt

--- init.sh
+++ init.sh
@@ -5,10 +5,10 @@
 # pip install django==2.0.0
 # pip install gunicorn==19.6.0
 #----------------------------------------------------
-sudo nginx -c /home/${USER}/web/config/nginx.conf
+sudo nginx -c "/home/${USER}/web/config/nginx.conf"
 gunicorn \
     --bind="0.0.0.0:8000" \
     --daemon \
-    --pid /home/${USER}/web/gunicorn.pid \
+    --pid "/home/${USER}/web/gunicorn.pid" \
     ask.wsgi
 
