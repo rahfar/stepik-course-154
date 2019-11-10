3993f4f2aa87b0a5f24e05c2ab824becd532ee3a
rahfar <rahfar777@gmail.com>
2019-11-10 23:45:22 +0300

    Modified models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,17 +1,24 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class Question(models.Model):
-    title = models.CharField(max_length=255)# - заголовок вопроса
-    text = models.TextField()# - полный текст вопроса
-    added_at = models.DateTimeField()# - дата добавления вопроса
-    rating = models.IntegerField()# - рейтинг вопроса (число)
-    #author = models.# - автор вопроса
-    #likes = models.# - список пользователей, поставивших "лайк"
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
 
-class Answer(models.Model): #  - ответ
-    text = models.TextField()# - текст ответа
-    added_at = models.DateTimeField()# - дата добавления ответа
-    question = models.# - вопрос, к которому относится ответ
-    author = models.# - автор ответа
+class Question(models.Model):
+    title = models.CharField(max_length=255) # - заголовок вопроса
+    text = models.TextField() # - полный текст вопроса
+    added_at = models.DateTimeField(auto_now_add=True) # - дата добавления вопроса
+    rating = models.IntegerField(default=0) # - рейтинг вопроса (число)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # - автор вопроса
+    likes = models.ManyToManyField(User, related_name='question_like_user') # - список пользователей, поставивших "лайк"
+    objects = QuestionManager() 
+
+class Answer(models.Model):
+    text = models.TextField() # - текст ответа
+    added_at = models.DateTimeField(auto_now_add=True) # - дата добавления ответа
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # - вопрос, к которому относится ответ
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # - автор ответа
