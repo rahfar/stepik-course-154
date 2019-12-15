6d493d28211fb1d1703bbcf211f990e7739fbd59
rahfar <rahfar777@gmail.com>
2019-12-15 22:34:27 +0300

    deleted:    ask/qa/fixtures/testdata.json 	modified:   ask/qa/fixtures/testdata.yaml 	modified:   make_pyenv.sh 	modified:   requirements.txt

--- make_pyenv.sh
+++ make_pyenv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 sudo apt-get install libmysqlclient-dev
+sudo apt-get install python-dev default-libmysqlclient-dev
 virtualenv -p python3 pyenv
 source pyenv/bin/activate
 pip install --upgrade pip
